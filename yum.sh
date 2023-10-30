d155399d4ca651eee4c9321d817f475b2390cb9b
1413626340 <68804826+1413626340@users.noreply.github.com>
2023-10-30 16:39:45 +0800

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -7,7 +7,7 @@ cat <<EOF>$Path/Base.repo
 [base]
 name=CentOS-$releasever - Base
 #mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra
-baseurl=file:///mnt/AppStream
+baseurl=file:///mnt
 gpgcheck=0
 #gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 enabled=1
